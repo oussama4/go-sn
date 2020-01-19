028a8a85d2b360d3c8a4e09d7e38a90deb639869
usama <oussamazouaki4@gmail.com>
2020-01-19 01:22:14 +0100

    add form validation package

---
+++ pkg/forms/form.go
@@ -0,0 +1,72 @@
+package forms
+
+import (
+	"fmt"
+	"net/url"
+	"regexp"
+	"strings"
+	"unicode/utf8"
+)
+
+// EmailRx is a regular expression pattern of a valid email address
+var EmailRx = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+
+// Form holds form data and the errors associated with it
+type Form struct {
+	url.Values
+	errors Errors
+}
+
+// New creates a form struct given the form data
+func New(data url.Values) *Form {
+	return &Form{
+		data,
+		Errors(map[string][]string{}),
+	}
+}
+
+// Required checks if a set of fields are not empty
+// if any field is empty add an error message to form errors
+func (f *Form) Required(fields ...string) {
+	for _, field := range fields {
+		value := f.Get(field)
+		if strings.TrimSpace(value) == "" {
+			f.errors.Add(field, "this field is required")
+		}
+	}
+}
+
+// MaxLength checks the number of characters in a field don't pass a maximum value
+func (f *Form) MaxLength(field string, max int) {
+	value := f.Get(field)
+	if value == "" {
+		return
+	}
+
+	if utf8.RuneCountInString(value) > max {
+		f.errors.Add(field, fmt.Sprintf("%s must not pass %d characters", field, max))
+	}
+}
+
+// MinLength checks  field has a minimum number of characters
+func (f *Form) MinLength(field string, min int) {
+	value := f.Get(field)
+	if value == "" {
+		return
+	}
+
+	if utf8.RuneCountInString(value) < min {
+		f.errors.Add(field, fmt.Sprintf("%s must have at least %d characters", field, min))
+	}
+}
+
+// ValidEmail validate an email address
+func (f *Form) ValidEmail(field string) {
+	value := f.Get(field)
+	if value == "" {
+		return
+	}
+	if !EmailRx.MatchString(value) {
+		f.errors.Add(field, "email is not valid")
+	}
+}
